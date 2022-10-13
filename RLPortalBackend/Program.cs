529584ddd4cab204153ee41baaca079aac481f61
DMutaliev <daniilmutaliev2001@gmail.com>
2022-10-13 21:21:17 +0400

    add mongoDB connection and DI for Exercise and Test Components

--- RLPortalBackend/Program.cs
+++ RLPortalBackend/Program.cs
@@ -1,11 +1,25 @@
+using RLPortal.Repositories.Impl;
+using RLPortal.Repositories;
+using RLPortal.Services.Impl;
+using RLPortal.Services;
+using RLPortal.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Connection to the MongoDB
+builder.Services.Configure<PortalGeographyMongoDBSettings>(
+    builder.Configuration.GetSection("RLPortalMongoDB"));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// DI
+builder.Services.AddScoped<IExerciseRepository, ExerciseRepository>();
+builder.Services.AddScoped<IExerciseService, ExerciseService>();
+builder.Services.AddScoped<ITestRepository, TestRepository>();
+builder.Services.AddScoped<ITestService, TestService>();
 
 var app = builder.Build();
 
