a61bf81a6ff9826aee523d188f549e0e2299442c
DMutaliev <daniilmutaliev2001@gmail.com>
2022-10-16 18:33:50 +0400

    add Mapping Profile

---
+++ RLPortalBackend/Mappers/AppMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using RLPortalBackend.Dto;
+using RLPortalBackend.Entities;
+using System;
+
+namespace RLPortalBackend.Mappers
+{
+    public class AppMappingProfile : Profile
+    {
+        public AppMappingProfile()
+        {
+            CreateMap<ExerciseDto, ExerciseEntity>().ReverseMap();//.IgnoreAllSourcePropertiesWithAnInaccessibleSetter();
+        }
+    }
+}
