a98c408c3fd19fbc8753356d4274039c809800ce
DMutaliev <daniilmutaliev2001@gmail.com>
2022-10-16 18:32:56 +0400

    rename Exercise to ExerciseEntity, Test to TestEntity and replcate them to Entities Package

---
+++ RLPortalBackend/Entities/ExerciseEntity.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.IdGenerators;
+
+namespace RLPortalBackend.Entities
+{
+    /// <summary>
+    /// Exercise Entity
+    /// </summary>
+    public class ExerciseEntity
+    {
+        [BsonId(IdGenerator = typeof(GuidGenerator))]
+        public Guid Id { get; set; }
+
+        public string Description { get; set; }
+
+        public ICollection<string> Answers { get; set; }
+
+        public int RightAnswer { get; set; }
+
+    }
+}
---
+++ RLPortalBackend/Entities/TestEntity.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.IdGenerators;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RLPortalBackend.Entities
+{
+    /// <summary>
+    /// Test Entity
+    /// </summary>
+    public class TestEntity
+    {
+        [BsonId(IdGenerator = typeof(GuidGenerator))]
+        public Guid Id { get; set; }
+
+        public IEnumerable<Guid> ExerciseIds { get; set; }
+    }
+}
