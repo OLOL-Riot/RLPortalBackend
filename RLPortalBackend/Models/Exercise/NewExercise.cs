f195255901ea2bfd8892699dd3ec921b0ab98dd5
DMutaliev <daniilmutaliev2001@gmail.com>
2022-10-24 23:48:51 +0400

    feat: add NewExercise Model

---
+++ RLPortalBackend/Models/Exercise/NewExercise.cs
@@ -0,0 +1,12 @@
+﻿namespace RLPortalBackend.Models.Exercise
+{
+    public class NewExercise
+    {
+        public string Description { get; set; }
+
+        public ICollection<string> Answers { get; set; }
+
+        public string RightAnswer { get; set; }
+
+    }
+}
