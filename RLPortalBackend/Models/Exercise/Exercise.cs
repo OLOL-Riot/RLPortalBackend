87ccfd5affa65a44e1c7a86eef52e06409490ea9
DMutaliev <daniilmutaliev2001@gmail.com>
2022-10-24 23:50:37 +0400

    feat: add field RightAnswer

--- RLPortalBackend/Models/Exercise/Exercise.cs
+++ RLPortalBackend/Models/Exercise/Exercise.cs
@@ -2,10 +2,12 @@
 {
     public class Exercise
     {
-        public Guid? Id { get; set; }
+        public Guid Id { get; set; }
 
         public string Description { get; set; }
 
         public ICollection<string> Answers { get; set; }
+
+        public string RightAnswer { get; set; }
     }
 }
