bd081a4c91b4f4f2b8e9eb1d9a5ad9b312336315
DMutaliev <daniilmutaliev2001@gmail.com>
2022-10-25 00:09:05 +0400

    feat: add NewTest Model

---
+++ RLPortalBackend/Models/Test/NewTest.cs
@@ -0,0 +1,11 @@
+﻿using RLPortalBackend.Models.Exercise;
+
+namespace RLPortalBackend.Models.Test
+{
+    public class NewTest
+    {
+        public string Name { get; set; }
+
+        public IEnumerable<NewExercise> Exercises { get; set; }
+    }
+}
