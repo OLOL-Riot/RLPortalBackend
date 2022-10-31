6e9b99b0e6b46dc2d7301dcd2f356262e8c39c65
Valentine <valmak29@gmail.com>
2022-10-31 21:03:41 +0400

    refactor

--- RLPortalBackend/Models/Test/CompletedTestResult.cs
+++ RLPortalBackend/Models/Test/CompletedTestResult.cs
@@ -5,10 +5,8 @@ namespace RLPortalBackend.Models.Test
 {
     public class CompletedTestResult
     {
-        [JsonPropertyName("Points")]
         public int Points { get; set; }
 
-        [JsonPropertyName("MaxPoints")]
         public int MaxPoints { get; set; }
 
         public ICollection<VerifiedExercise> VerifiedAnswers { get; set; }
