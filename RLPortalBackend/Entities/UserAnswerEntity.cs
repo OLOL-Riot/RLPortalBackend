c9f247ecd844a705831398b75841bcf94618fc7f
DMutaliev <daniilmutaliev2001@gmail.com>
2022-11-01 18:53:35 +0400

    feat: add Entity Models

---
+++ RLPortalBackend/Entities/UserAnswerEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace RLPortalBackend.Entities
+{
+    public class UserAnswerEntity
+    {
+        public Guid ExerciseId { get; set; }
+
+        public string ChosenAnswer { get; set; }
+
+        public bool IsRight { get; set; }
+    }
+}
---
+++ RLPortalBackend/Entities/VerifiedTestEntity.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.IdGenerators;
+
+namespace RLPortalBackend.Entities
+{
+    public class VerifiedTestEntity
+    {
+        [BsonId(IdGenerator = typeof(GuidGenerator))]
+        public Guid Id { get; set; }
+
+        public Guid UserId { get; set; }
+
+        public Guid TestId { get; set; }
+
+        public int Points { get; set; }
+
+        public int MaxPoints { get; set; }
+
+        public ICollection<UserAnswerEntity> UserAnswers { get; set; }
+
+    }
+}
