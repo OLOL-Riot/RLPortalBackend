745c458d3275e40e5e518b745c8609a7304915fa
DMutaliev <daniilmutaliev2001@gmail.com>
2022-11-01 20:47:54 +0400

    feat: add VerifiedTestService

--- RLPortalBackend/Services/Impl/VerifiedTestService.cs
+++ RLPortalBackend/Services/Impl/VerifiedTestService.cs
@@ -1,6 +1,37 @@
-﻿namespace RLPortalBackend.Services.Impl
+﻿using RLPortalBackend.Models.VerifiedTest;
+
+namespace RLPortalBackend.Services.Impl
 {
-    public class VerifiedTestService
+    public class VerifiedTestService : IVerifiedTestService
     {
+        public Task CreateAsync(SolvedTestDto solvedTest, Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<VerifiedTestDto>> GetAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<VerifiedTestDto> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<VerifiedTestDto>> GetByUserIdAsync(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Guid id, VerifiedTestDto updatedVerifiedTestDto)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
