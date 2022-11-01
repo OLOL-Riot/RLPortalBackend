f5365f62aec5c1de3be2a9b9fac10fe4fa578776
DMutaliev <daniilmutaliev2001@gmail.com>
2022-11-01 22:05:33 +0400

    feat: create controller for Verified Test

---
+++ RLPortalBackend/Controllers/VerifiedTestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RLPortalBackend.Services;
+
+namespace RLPortalBackend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class VerifiedTestController: Controller
+    {
+        private readonly IVerifiedTestService _verifiedTestService;
+
+        public VerifiedTestController(IVerifiedTestService verifiedTestService)
+        {
+            _verifiedTestService = verifiedTestService;
+        }
+    }
+}
