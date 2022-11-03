f1bc14e16b3bea261c506929f6338f28e205608c
Danilos-Prog <daniilmutaliev2001@gmail.com>
2022-11-04 00:40:37 +0400

    docs: entities

--- RLPortalBackend/Entities/VerifiedExerciseEntity.cs
+++ RLPortalBackend/Entities/VerifiedExerciseEntity.cs
@@ -1,11 +1,28 @@
-﻿namespace RLPortalBackend.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RLPortalBackend.Entities
 {
+    /// <summary>
+    /// VerifiedExerciseEntity
+    /// </summary>
     public class VerifiedExerciseEntity
     {
+        /// <summary>
+        /// ExerciseId
+        /// </summary>
+        [Required]
         public Guid ExerciseId { get; set; }
 
+        /// <summary>
+        /// ChosenAnswer
+        /// </summary>
+        [Required]
         public string ChosenAnswer { get; set; }
 
+        /// <summary>
+        /// IsRight
+        /// </summary>
+        [Required]
         public bool IsRight { get; set; }
     }
 }
--- RLPortalBackend/Entities/VerifiedTestEntity.cs
+++ RLPortalBackend/Entities/VerifiedTestEntity.cs
@@ -1,23 +1,55 @@
 ﻿using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Bson.Serialization.IdGenerators;
+using System.ComponentModel.DataAnnotations;
 
 namespace RLPortalBackend.Entities
 {
+    /// <summary>
+    /// VerifiedTestEntity
+    /// </summary>
     public class VerifiedTestEntity
     {
+        /// <summary>
+        /// Id
+        /// </summary>
+        [Required]
         [BsonId(IdGenerator = typeof(GuidGenerator))]
         public Guid Id { get; set; }
 
+        /// <summary>
+        /// UserId
+        /// </summary>
+        [Required]
         public Guid UserId { get; set; }
 
+        /// <summary>
+        /// TestId
+        /// </summary>
+        [Required]
         public Guid TestId { get; set; }
 
+        /// <summary>
+        /// VerifyTestDateTime
+        /// </summary>
+        [Required]
         public DateTimeOffset VerifyTestDateTime { get; set; }
 
+        /// <summary>
+        /// Points
+        /// </summary>
+        [Required]
         public int Points { get; set; }
 
+        /// <summary>
+        /// MaxPoints
+        /// </summary>
+        [Required]
         public int MaxPoints { get; set; }
 
+        /// <summary>
+        /// VerifiedAnswers
+        /// </summary>
+        [Required]
         public ICollection<VerifiedExerciseEntity> VerifiedAnswers { get; set; }
 
     }
