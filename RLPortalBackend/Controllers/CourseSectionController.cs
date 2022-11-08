bf2d9c411c699da7c4ae0a1819a1c0a7b2e67c8d
Valentine <valmak29@gmail.com>
2022-11-08 18:59:19 +0400

    refactor-feat: add comments and update

--- RLPortalBackend/Controllers/CourseSectionController.cs
+++ RLPortalBackend/Controllers/CourseSectionController.cs
@@ -1,11 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using RLPortalBackend.Entities;
 using RLPortalBackend.Models.CourseSection;
 using RLPortalBackend.Services;
 
 namespace RLPortalBackend.Controllers
 {
-
+    /// <summary>
+    /// CourseSectionController
+    /// </summary>
     [ApiController]
     [Route("api/[controller]")]
     public class CourseSectionController : ControllerBase
@@ -18,18 +21,46 @@ namespace RLPortalBackend.Controllers
             _courseSectionService = courseSectionService;
         }
 
+        /// <summary>
+        /// Get all CourseSection
+        /// (Permissions: User, Administrator)
+        /// </summary>
+        /// <returns></returns>
+        [ProducesResponseType(typeof(ICollection<CourseSectionDto>), 200)]
+
+        [Authorize(Roles = "User, Administrator")]
         [HttpGet("get")]
         public async Task<ICollection<CourseSectionDto>> GetCourseSectionDtosAsync()
         {
             return await _courseSectionService.GetAsync();
         }
 
+        /// <summary>
+        /// Get CourseSection by Id
+        /// (Permissions: User, Administrator)
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [ProducesResponseType(typeof(CourseSectionDto), 200)]
+        [ProducesResponseType(404)]
+
+        [Authorize(Roles = "User, Administrator")]
         [HttpGet("get/{id:length(36)}")]
         public async Task<CourseSectionDto> GetCourseSectionByIdAsync(Guid id)
         {
             return await _courseSectionService.GetByIdAsync(id);
         }
 
+        /// <summary>
+        /// Delete CourseSection by Id
+        /// (Permissions: Administrator)
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [ProducesResponseType(204)]
+        [ProducesResponseType(404)]
+
+        [Authorize(Roles = "Administrator")]
         [HttpDelete("remove/{id:length(36)}")]
         public async Task<ActionResult> RemoveCourseSectionById(Guid id)
         {
@@ -37,19 +68,56 @@ namespace RLPortalBackend.Controllers
             return NoContent();
         }
 
+        /// <summary>
+        /// Create CourseSection
+        /// (Permissions: Administrator)
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        [ProducesResponseType(typeof(CourseSectionDto), 201)]
+
+        [Authorize(Roles = "Administrator")]
         [HttpPost("create")]
-        public async Task<CourseSectionDto> CreateNewCourseSection([FromBody] NewCourseSectionDto input)
+        public async Task<ActionResult<CourseSectionDto>> CreateNewCourseSection([FromBody] NewCourseSectionDto input)
         {
-            return await _courseSectionService.CreateAsync(input);
+            var dto = await _courseSectionService.CreateAsync(input);
+            return CreatedAtAction(nameof(CreateNewCourseSection), dto);
         }
 
+        /// <summary>
+        /// GetPreviews
+        /// (Permissions: User, Administrator)
+        /// </summary>
+        /// <param name="ids"></param>
+        /// <returns></returns>
+        [ProducesResponseType(typeof(ICollection<PreviewCourseDto>), 200)]
+
+        [Authorize(Roles = "User, Administrator")]
         [HttpPost("preview")]
         public async Task<ICollection<PreviewCourseDto>> GetPreviews(ICollection<Guid> ids)
         {
             return await _courseSectionService.GetPreviewCourse(ids);
         }
 
-        
+        /// <summary>
+        /// Update CourseSection by Id
+        /// (Permissions: User, Administrator)
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="newCourseSectionDto"></param>
+        /// <returns></returns>
+        [ProducesResponseType(204)]
+        [ProducesResponseType(404)]
+
+        [Authorize(Roles = "User, Administrator")]
+        [HttpPut("update/{id:length(36)}")]
+        public async Task<ActionResult> UpdateCourseSection(Guid id, NewCourseSectionDto newCourseSectionDto)
+        {
+            await _courseSectionService.UpdateAsync(id, newCourseSectionDto);
+            return NoContent();
+        }
+
+
 
 
 
