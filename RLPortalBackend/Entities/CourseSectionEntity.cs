b1e7bfbd2a64e371a886e2797e0db5ce67ad0713
Valentine <valmak29@gmail.com>
2022-11-08 07:20:09 +0400

    refactor: add guid generator

--- RLPortalBackend/Entities/CourseSectionEntity.cs
+++ RLPortalBackend/Entities/CourseSectionEntity.cs
@@ -1,11 +1,25 @@
-﻿namespace RLPortalBackend.Entities
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.IdGenerators;
+using System.ComponentModel.DataAnnotations;
+
+namespace RLPortalBackend.Entities
 {
     public class CourseSectionEntity
     {
+        [BsonId(IdGenerator = typeof(GuidGenerator))]
+        [Required]
         public Guid Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+        
+        [Required]
         public string ShortDescription { get; set; }
+
+        [Required]
         public ICollection<Guid> TheoryEntityId { get; set; }
+
+        [Required]
         public ICollection<Guid> TestEntityId { get; set; }
 
     }
