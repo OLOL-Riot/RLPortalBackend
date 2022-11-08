45541d07d411948935eedd3aade8c946d4b3be81
Valentine <valmak29@gmail.com>
2022-11-08 06:57:48 +0400

    feat: Theory and TheorySection Entity

---
+++ RLPortalBackend/Entities/TheoryEntity.cs
@@ -0,0 +1,44 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.IdGenerators;
+using System.ComponentModel.DataAnnotations;
+
+namespace RLPortalBackend.Entities
+{
+    /// <summary>
+    /// TheoryEntity
+    /// </summary>
+    public class TheoryEntity
+    {
+
+        /// <summary>
+        /// Id
+        /// </summary>
+        [Required]
+        [BsonId(IdGenerator = typeof(GuidGenerator))]
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Name
+        /// </summary>
+        [Required]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Description
+        /// </summary>
+        [Required]
+        public string Description { get; set; }
+
+        /// <summary>
+        /// ShortDescription
+        /// </summary>
+        [Required]
+        public string ShortDescription { get; set; }
+
+        /// <summary>
+        /// TheorySectionEntities
+        /// </summary>
+        [Required]
+        public ICollection<TheorySectionEntity> TheorySectionEntities { get; set; }
+    }
+}
---
+++ RLPortalBackend/Entities/TheorySectionEntity.cs
@@ -0,0 +1,32 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.IdGenerators;
+using System.ComponentModel.DataAnnotations;
+
+namespace RLPortalBackend.Entities
+{
+    /// <summary>
+    /// Theory section entity
+    /// </summary>
+    public class TheorySectionEntity
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        [BsonId(IdGenerator = typeof(GuidGenerator))]
+        [Required]
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Header
+        /// </summary>
+        [Required]
+        public string Header { get; set; }
+
+        /// <summary>
+        /// Content
+        /// </summary>
+        [Required]
+        public string Content { get; set; }
+
+    }
+}
