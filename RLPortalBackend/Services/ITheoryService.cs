6e7f63402c5a41f1a8b2342bc288050ef531f5c5
Valentine <valmak29@gmail.com>
2022-11-08 15:45:38 +0400

    refactor: add comments

--- RLPortalBackend/Services/ITheoryService.cs
+++ RLPortalBackend/Services/ITheoryService.cs
@@ -5,14 +5,39 @@ namespace RLPortalBackend.Services
 {
     public interface ITheoryService
     {
+        /// <summary>
+        /// Create Theory
+        /// </summary>
+        /// <param name="theoryDto"></param>
+        /// <returns></returns>
         public Task<TheoryDto> CreateAsync(NoIdTheoryDto theoryDto);
 
+        /// <summary>
+        /// Get all Theory
+        /// </summary>
+        /// <returns></returns>
         public Task<ICollection<TheoryDto>> GetAsync();
 
+        /// <summary>
+        /// Get Theory by Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public Task<TheoryDto> GetByIdAsync(Guid id);
 
+        /// <summary>
+        /// Update Theory by Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="updateTheoryDto"></param>
+        /// <returns></returns>
         public Task UpdateAsync(Guid id, NoIdTheoryDto updateTheoryDto);
 
+        /// <summary>
+        /// Remove Theory by Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public Task RemoveAsync(Guid id);
 
     }
