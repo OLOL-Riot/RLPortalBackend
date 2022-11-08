9f1a2d9af9db586bb4562c5a868f6f6ba479b85b
Valentine <valmak29@gmail.com>
2022-11-08 06:58:53 +0400

    feat: repo for theory

---
+++ RLPortalBackend/Repositories/ITheoryRepository.cs
@@ -0,0 +1,24 @@
+﻿using RLPortalBackend.Entities;
+
+namespace RLPortalBackend.Repositories
+{
+    /// <summary>
+    /// ITheoryRepository
+    /// </summary>
+    public interface ITheoryRepository
+    {
+        public Task CreateAsync(TheoryEntity newTheory);
+
+        public Task<ICollection<TheoryEntity>> GetAsync();
+
+        public Task<TheoryEntity> GetAsync(Guid id);
+
+        public Task RemoveAsync(Guid id);
+
+        public Task UpdateAsync(Guid id, TheoryEntity updatedTheory);
+
+
+
+
+    }
+}
