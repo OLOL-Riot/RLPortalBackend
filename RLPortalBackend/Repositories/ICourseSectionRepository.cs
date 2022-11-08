19879229afe60bf514bf9bc2dc4d97a1e435fbaf
Valentine <valmak29@gmail.com>
2022-11-08 07:21:19 +0400

    feat: repo for course section

---
+++ RLPortalBackend/Repositories/ICourseSectionRepository.cs
@@ -0,0 +1,17 @@
+﻿using RLPortalBackend.Entities;
+
+namespace RLPortalBackend.Repositories
+{
+    public interface ICourseSectionRepository
+    {
+        public Task CreateAsync(CourseSectionEntity newCourseSectionEntity);
+
+        public Task<ICollection<CourseSectionEntity>> GetAsync();
+
+        public Task<CourseSectionEntity> GetAsync(Guid id);
+
+        public Task RemoveAsync(Guid id);
+
+        public Task UpdateAsync(Guid id, CourseSectionEntity updateCourseSectionEntity);
+    }
+}
