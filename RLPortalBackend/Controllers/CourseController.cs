4236c486f8e790b35d8d6643b6a0aec5a28257c0
Danilos-Prog <daniilmutaliev2001@gmail.com>
2022-11-09 00:21:37 +0400

    feat(CourseController): add base structure

---
+++ RLPortalBackend/Controllers/CourseController.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RLPortalBackend.Models.Course;
+using RLPortalBackend.Models.CourseSection;
+using RLPortalBackend.Services;
+using RLPortalBackend.Services.Impl;
+
+namespace RLPortalBackend.Controllers
+{
+    /// <summary>
+    /// CourseController
+    /// </summary>
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CourseController: Controller
+    {
+        private readonly ICourseService _courseService;
+
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        /// <summary>
+        /// Get all courses 
+        /// (Permissions: User, Administrator)
+        /// </summary>
+        /// <returns></returns>
+        [ProducesResponseType(typeof(ICollection<CourseDto>), 200)]
+
+        [Authorize(Roles = "User, Administrator")]
+        [HttpGet]
+        public async Task<ICollection<CourseDto>> GetCoursesAsync()
+        {
+
+        }
+
+        /// <summary>
+        /// Get course by id 
+        /// (Permissions: User, Administrator)
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [ProducesResponseType(typeof(CourseDto), 200)]
+
+        [Authorize(Roles = "User, Administrator")]
+        [HttpGet("{id:length(36)}")]
+        public async Task<CourseDto> GetCourseByIdAsync(Guid id)
+        {
+
+        }
+
+        /// <summary>
+        /// Get all preview courses 
+        /// (Permissions: User, Administrator)
+        /// </summary>
+        /// <returns></returns>
+        [ProducesResponseType(typeof(ICollection<PreviewCourseDto>), 200)]
+
+        [Authorize(Roles = "User, Administrator")]
+        [HttpGet]
+        public async Task<ICollection<PreviewCourseDto>> GetPreviewCoursesAsync()
+        {
+
+        }
+
+        /// <summary>
+        /// Create Course 
+        /// (Permissions: Administrator)
+        /// </summary>
+        /// <param name="createCourseDto"></param>
+        /// <returns></returns>
+        [ProducesResponseType(typeof(CourseDto), 201)]
+
+        [Authorize(Roles = "Administrator")]
+        [HttpPost]
+        public async Task<ActionResult<CourseDto>> CreateNewCourse([FromBody] CreateCourseDto createCourseDto)
+        {
+            
+        }
+
+        /// <summary>
+        /// Update Course by Id
+        /// (Permissions: Administrator)
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="updateCourseDto"></param>
+        /// <returns></returns>
+        [ProducesResponseType(204)]
+        [ProducesResponseType(404)]
+
+        [Authorize(Roles = "User, Administrator")]
+        [HttpPut("{id:length(36)}")]
+        public async Task<ActionResult> UpdateCourseSection(Guid id, UpdateCourseDto updateCourseDto)
+        {
+
+        }
+
+        /// <summary>
+        /// Delete Course by Id 
+        /// (Permissions: Administrator)
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [ProducesResponseType(204)]
+        [ProducesResponseType(404)]
+
+        [Authorize(Roles = "Administrator")]
+        [HttpDelete("{id:length(36)}")]
+        public async Task<ActionResult> RemoveCourseById(Guid id)
+        {
+
+        }
+
+    }
+}
