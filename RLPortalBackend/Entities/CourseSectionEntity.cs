dc9e21549831a0275296a6ae0a7147ee4a5fb454
Valentine <valmak29@gmail.com>
2022-11-08 07:05:41 +0400

    feat: CourseSectionEntity

---
+++ RLPortalBackend/Entities/CourseSectionEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace RLPortalBackend.Entities
+{
+    public class CourseSectionEntity
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string ShortDescription { get; set; }
+        public ICollection<Guid> TheoryEntityId { get; set; }
+        public ICollection<Guid> TestEntityId { get; set; }
+
+    }
+}
