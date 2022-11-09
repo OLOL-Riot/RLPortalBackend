52529a4165b4f90cb6356da58c12600fa8ef28c3
Danilos-Prog <daniilmutaliev2001@gmail.com>
2022-11-09 14:46:55 +0400

    feat(CourseEntity): implement

--- RLPortalBackend/Entities/CourseEntity.cs
+++ RLPortalBackend/Entities/CourseEntity.cs
@@ -1,6 +1,33 @@
 ﻿namespace RLPortalBackend.Entities
 {
+    /// <summary>
+    /// CourseEntity
+    /// </summary>
     public class CourseEntity
     {
+        /// <summary>
+        /// Id
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Description
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// ShortDescription
+        /// </summary>
+        public string ShortDescription { get; set; }
+
+        /// <summary>
+        /// CourseSectionEntityIds
+        /// </summary>
+        public ICollection<Guid> CourseSectionEntityIds { get; set; }
     }
 }
