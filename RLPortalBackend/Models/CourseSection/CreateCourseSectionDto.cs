69ac58d4379d37036e2960fe24137cdaf9265f1e
Valentine <valmak29@gmail.com>
2022-11-27 18:38:03 +0400

    feat: change create and update course section

---
+++ RLPortalBackend/Models/CourseSection/CreateCourseSectionDto.cs
@@ -0,0 +1,25 @@
+﻿namespace RLPortalBackend.Models.CourseSection
+{
+    public class CreateCourseSectionDto
+    {
+        /// <summary>
+        /// Name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// ShortDescription
+        /// </summary>
+        public string ShortDescription { get; set; }
+
+        /// <summary>
+        /// SerialNumber
+        /// </summary>
+        public int SerialNumber { get; set; }
+
+        /// <summary>
+        /// Course id
+        /// </summary>
+        public Guid? CourseId { get; set; }
+    }
+}
