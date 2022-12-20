6efabef1962d10d33c57d366e58f31301957a615
Valentine <valmak29@gmail.com>
2022-12-21 00:02:29 +0400

    feat: change confirm email from get to post

---
+++ RLPortalBackend/Models/Autentification/ConfirmEmailDto.cs
@@ -0,0 +1,8 @@
+﻿namespace RLPortalBackend.Models.Autentification
+{
+    public class ConfirmEmailDto
+    {
+        public Guid Id { get; set; }
+        public string Token { get; set; }
+    }
+}
